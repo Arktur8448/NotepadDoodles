03992a4481bda4519123e69a38270d017db48416
Artur <amergalski@zsnr1.com>
2024-04-15 19:32:47 +0200

    v0.2

--- characters.py
+++ characters.py
@@ -1,9 +1,14 @@
+import items
+
+
 class Character:
     def __init__(self, name="", desc="",
-                 base_hp=1, base_hp_regen_rate=1, base_stamina=1, base_stamina_regen_rate=1,
-                 base_strength=1, base_defence=1, base_agility=1, base_dodge=1, base_accuracy=1,
-                 base_movement_speed=1,
-                 base_dash_distance=1, base_dash_cooldown=1, base_dash_duration=1):
+                 base_hp=1.0, base_hp_regen_rate=1.0, base_stamina=1.0, base_stamina_regen_rate=1.0,
+                 base_strength=1.0, base_defence=1.0, base_agility=1.0, base_dodge=1.0, base_accuracy=1.0,
+                 base_movement_speed=1.0,
+                 base_dash_distance=1.0, base_dash_cooldown=1.0, base_dash_duration=1.0, starter_weapons=None):
+        if starter_weapons is None:
+            starter_weapons = []
         self.name = name
         self.desc = desc
         self.detailed_desc_addition = None
@@ -24,6 +29,7 @@ class Character:
         self.base_dash_distance = base_dash_distance
         self.base_dash_cooldown = base_dash_cooldown
         self.base_dash_duration = base_dash_duration
+        self.starter_weapons = starter_weapons
 
     def apply_multiplayer(self, playerObject):
         playerObject.max_hp *= self.base_hp
@@ -34,12 +40,16 @@ class Character:
         playerObject.strength *= self.base_strength
         playerObject.defence *= self.base_defence
         playerObject.agility *= self.base_agility
+        playerObject.dodge *= self.base_dodge
+        playerObject.accuracy *= self.base_accuracy
 
         playerObject.movement_speed *= self.base_movement_speed
 
         playerObject.dash_distance *= self.base_dash_distance
         playerObject.dash_cooldown *= self.base_dash_cooldown
         playerObject.dash_duration *= self.base_dash_duration
+
+        playerObject.weapons = self.starter_weapons
 
     def character_start_skills(self, playerObject):
         """
@@ -117,12 +127,17 @@ class Character:
             sign = "+" if dash_cooldown_modifier >= 0 else "-"
             desc += f"Dash Cooldown: {sign}{abs(dash_cooldown_modifier)}%\n"
         if self.base_dash_duration != 1:
-            dash_duration_modifier = (self.base_dash_duration * 100 - 100)
-            sign = "+" if dash_duration_modifier >= 0 else "-"
-            desc += f"Dash Duration: {sign}{abs(dash_duration_modifier)}%\n"
+            dash_duration_modifier = (self.base_dash_duration * 100)
+            sign = "-" if dash_duration_modifier <= 0 else "+"
+            desc += f"Dash Speed: {sign}{abs(dash_duration_modifier)}%\n"
 
         if self.detailed_desc_addition is not None:
-            desc += self.detailed_desc_addition
+            desc += f"{self.detailed_desc_addition} \n"
+
+        weapon_list = f"Weapons: "
+        for w in self.starter_weapons:
+            weapon_list += w.name + " "
+        desc += f"{weapon_list} \n"
 
         return desc
 
@@ -132,6 +147,7 @@ class StickMan(Character):
         self.name = "StickMan"
         self.desc = "The most well rounded character with basic skills and powers"
         super().__init__(self.name, self.desc)
+        self.starter_weapons = [items.Weapon("sprites/gui/bars/Bar.png", "Sword", damage=10)]
 
 
 class Golem(Character):
@@ -142,6 +158,8 @@ class Golem(Character):
                          base_hp_regen_rate=0.5,
                          base_movement_speed=0.5, base_agility=0.25, base_dodge=0, base_strength=2)
         self.detailed_desc_addition = "Cannot Sprint or Dash or Dodge\nCannot regenerate any stamina"
+        self.starter_weapons = [
+            items.Weapon("sprites/gui/bars/Bar.png", "Great Sword", damage=20, speed=2, attack_range=2, slash_scale=2)]
 
     def character_skills(self, playerObject):
         playerObject.stamina = 0
@@ -154,29 +172,57 @@ class Warrior(Character):
         self.desc = "The mighty warrior. Have good defence and stamina. He is a bit slow. He is very good close range fighter but he have problems with ranged weapons."
         super().__init__(self.name, self.desc, base_hp=1.25, base_defence=1.75, base_stamina=2,
                          base_stamina_regen_rate=2,
-                         base_movement_speed=0.75, base_dash_cooldown=0.75,
+                         base_movement_speed=0.75, base_dash_cooldown=0.75, base_dash_duration=1.25,
                          base_accuracy=0.2, base_dodge=1.25,
                          base_strength=1.25)
-        self.detailed_desc_addition = ""
+        self.starter_weapons = [
+            items.Weapon("sprites/gui/bars/Bar.png", "Axe", damage=15, speed=1.5, attack_range=1, slash_scale=1.5)]
 
 
 class Ranger(Character):
     def __init__(self):
         self.name = "Ranger"
         self.desc = "The the accurate sniper. Have have high accuracy and dodge. But he is very vulnerable and have no clue how to use close-range weapons."
-        super().__init__(self.name, self.desc, base_hp=0.5, base_defence=0.5, base_stamina=1.5,
+        super().__init__(self.name, self.desc, base_hp=0.5, base_defence=0.25, base_stamina=1.5,
                          base_stamina_regen_rate=1.5,
-                         base_movement_speed=1.25, base_dash_distance=2, base_dash_cooldown=0.5, base_dash_duration=0.5,
+                         base_movement_speed=1.25, base_dash_distance=2, base_dash_cooldown=0.75,
+                         base_dash_duration=0.5,
                          base_accuracy=2, base_dodge=2, base_agility=1.25, base_strength=0)
-        self.detailed_desc_addition = "Cannot use close-range weapons\n"
+        self.detailed_desc_addition = "Cannot use close-range weapons"
+        self.starter_weapons = [
+            items.RangedWeapon("sprites/gui/bars/Bar.png", "sprites/weapons/bullets/arrow.png", "Bow", damage=15,
+                               speed=1.5, attack_range=2)]
+
+    def character_skills(self, playerObject):
+        playerObject.strength = 0
 
 
 class Wizard(Character):
     def __init__(self):
         self.name = "Wizard"
         self.desc = "The the wise wizard. He cast powerful spells. He despise other weapons than spells."
-        super().__init__(self.name, self.desc, base_hp=0.75, base_stamina=1.5,
+        super().__init__(self.name, self.desc, base_hp=0.75, base_stamina=1.25,
                          base_stamina_regen_rate=1.25,
-                         base_movement_speed=1.5,
+                         base_movement_speed=1.3,
                          base_dodge=1.25, base_agility=2, base_strength=0, base_accuracy=0)
         self.detailed_desc_addition = "Cannot use close range weapons\nCannot use ranged weapons"
+        self.starter_weapons = [
+            items.Wand("sprites/gui/bars/Bar.png", "sprites/weapons/bullets/magic_bullet.png", "Wand", damage=10, speed=1, attack_range=2)]
+
+    def character_skills(self, playerObject):
+        playerObject.strength = 0
+        playerObject.accuracy = 0
+
+
+class Thief(Character):
+    def __init__(self):
+        self.name = "Thief"
+        self.desc = "The sneaky thief. He is very fast and skilled, but has little resistance. He usually runs away in difficult situations."
+
+        super().__init__(self.name, self.desc, base_hp=0.3, base_stamina=1.5,
+                         base_stamina_regen_rate=1.5,
+                         base_movement_speed=1.5, base_dash_distance=1.5, base_dash_cooldown=0.5,
+                         base_dash_duration=0.25,
+                         base_dodge=1.5, base_defence=0.2, base_agility=1.25, base_strength=0.75, base_accuracy=1.25)
+        self.starter_weapons = [
+            items.Weapon("sprites/gui/bars/Bar.png", "Dagger", damage=7, speed=0.5, attack_range=0.75)]
